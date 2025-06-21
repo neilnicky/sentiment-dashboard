0081f70efbd0cebd37d54cb300ceea213e9a4a2f
neilnicky <neilnicholaskanat@gmail.com>
2025-06-21 12:42:41 +0530

    feat: install project dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SentimentForm from "./components/SentimentForm";
+
+function App() {
+  return (
+    <div>
+      <SentimentForm />
+    </div>
+  );
+}
+
+export default App;
